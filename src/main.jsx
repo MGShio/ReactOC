e4241d386712b0df14afd2620b8d0319cd98ce42
MGShio <maxenceguenaou@yahoo.ca>
2024-06-13 16:33:05 +0200

    Correction css et path
    Correction couleur et opacité css et pathing vers 404

--- src/main.jsx
+++ src/main.jsx
@@ -6,26 +6,16 @@ import {
 } from "react-router-dom";
 import App from './App.jsx'
 import './main.css'
+import { BrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <h1>404 not found</h1>
-  },
-  {
-    path: "/pages/apartment",
-    element: <h1>Nos appartements</h1>
-  },
-  {
-    path: "/pages/about",
-    element: <h1>A propos</h1>
-  },
-  {
-    path: "/pages/error",
-    element: <h1>404 Oups! La page que vous demandez n'existe pas</h1>
-  },
-]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
