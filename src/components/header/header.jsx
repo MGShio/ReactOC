76cad6e2ee9403b42b15c3410b3419ca6b72016b
MGShio <maxenceguenaou@yahoo.ca>
2024-06-20 16:22:10 +0200

    Correction style + ajout logo clickable
    Logo -> accueil
    Correction style

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,9 @@ import { NavLink } from "react-router-dom";
 function Header() {
     return (
         <header className="header">
-            <img className="header__logo" src="../logo.svg" alt="Logo" />
+            <NavLink to="/" classname="header__logo__link">
+                <img className="header__logo" src="../logo.svg" alt="Logo" />
+            </NavLink>
             <nav className="header__navbar">
                 <NavLink className={state => state.isActive ? "header__navbar__link active" : "header__navbar__link"} to="/">Accueil</NavLink>
                 <NavLink className={state => state.isActive ? "header__navbar__link active" : "header__navbar__link"} to="/about">A propos</NavLink>
